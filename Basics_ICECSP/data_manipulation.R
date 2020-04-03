6f78a2054a1378ae442c6141a197435980776377
Anandu R <ananduraj32@gmail.com>
2020-04-03 11:20:02 +0530

    Update data_manipulation.R

--- Basics_ICECSP/data_manipulation.R
+++ Basics_ICECSP/data_manipulation.R
@@ -2,8 +2,13 @@
 # ------------------------- Data Manipulation --------------------------------------#
 # DPLYR
 install.packages("dplyr");
-
-
+library(dplyr)
+dplyr::filter
+# ?stats::filter
+# optional
+# install.packages("tidyverse");
+install.packages("nycflights13");
+library(nycflights13)
 
 
 
