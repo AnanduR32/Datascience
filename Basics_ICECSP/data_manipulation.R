6a241140c4fb556f435a3757b1996205a94ee22b
Anandu R <ananduraj32@gmail.com>
2020-04-03 12:01:09 +0530

    Update data_manipulation.R

--- Basics_ICECSP/data_manipulation.R
+++ Basics_ICECSP/data_manipulation.R
@@ -19,26 +19,27 @@ View(flights)
 str(flights)
 nrow(flights)
 
-## Performing Operations on the dataset 
+# Performing Operations on the dataset 
 
 ## select
 
 View(select(flights,carrier,arr_delay,dep_delay))
 
-## arrange -arrange the dataset in the ascending or descending order with respect to a particular parameter
-## It is used for the sorting of the data
+## arrange 
 
-## Ascending order
+### arrange the dataset in the ascending or descending order with respect to a particular parameter
+### It is used for the sorting of the data
+
+### Ascending order
 asc_arr_delay = arrange(flights,arr_delay)
 View(asc_arr_delay)
-## Descending order
+### Descending order
 desc_arr_delay = arrange(flights,desc(arr_delay))
 View(desc_arr_delay)
 
-## Write code to print vairables carrier, arr_delay and dep_delay 
-## which are sorted by dep_delay in descending order
+### Write code to print vairables carrier, arr_delay and dep_delay 
+### which are sorted by dep_delay in descending order
 View(arrange(select(flights,carrier,arr_delay,dep_delay),desc(dep_delay)))
-View(select(arrange(flights,desc(dep_delay)),carrier,arr_delay,dep_delay))
 
 ## distinct
 
@@ -46,6 +47,24 @@ View(select(arrange(flights,desc(dep_delay)),carrier,arr_delay,dep_delay))
 
 View(filter(flights,carrier=="UA"))
 View(filter(flights,arr_delay>1000))
+View(filter(flights,arr_delay>400,carrier=="UA"))
+View(filter(flights,arr_delay>450 | (carrier=="UA" & arr_delay>300)))
+
+### Question : write a code to print variables carrier, arr_delay and dep_delay
+### which are sorted by dep_delay in descending orer 
+### and shows the data for airlines UA and MQ only
+View(
+  arrange(
+    select(
+      filter(flights,carrier == "UA" | carrier == "MQ"),
+      carrier,arr_delay,dep_delay),
+    desc(dep_delay)
+    )
+  )
+
+### pipe operator  %>%
+flights %>% filter(carrier == "UA" | carrier == "MQ") %>% select(carrier,arr_delay,dep_delay) %>% arrange(desc(dep_delay)) %>% View
+
 
 ## summarise
 
