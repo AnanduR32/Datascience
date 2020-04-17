c827bb5dd6f792119239527d56a06dbc8f6f88b5
Anandu R <ananduraj32@gmail.com>
2020-04-17 20:15:20 +0530

    Update solution.R
    Hospital part 2 complete

--- Case Studies/hospital_care_flatlines/solution.R
+++ Case Studies/hospital_care_flatlines/solution.R
@@ -1,4 +1,5 @@
-##
+## Set Directory
+setwd(setwd_hospital)
 
 library(dplyr)
 
@@ -53,8 +54,41 @@ best = function(state, outcome) {
 
 rankhospital <- function(state, outcome, num = "best") {
   ## Read outcome data
+  dataset = read.csv("outcome-of-care-measures.csv",colClasses = "character")
+  if(sum(dataset$State==state)<1) stop("Invalid State")
+  dataset = dataset[dataset$State==state,]
+  
+  if(tolower(outcome) == "heart attack"){
+    dataset = select(dataset,Hospital.Name,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)
+  }else if(tolower(outcome) == "heart failure"){
+    dataset = select(dataset,Hospital.Name,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)
+  }else if(tolower(outcome) == "pneumonia"){
+    dataset = select(dataset,Hospital.Name,Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
+  }else{
+    stop("Invalid Outcome")
+  }
+  dataset[,2]=as.numeric(dataset[,2])
+  names(dataset) =  c("Name","Outcome")
   ## Check that state and outcome are valid
-  ## Return hospital name in that state with the given rank
-  ## 30-day death rate
+  dataset = dataset[complete.cases(dataset),]
+  Not_avail = dataset[,2]=="Not Available"
+  dataset = dataset[!Not_avail,]
+  
+  ## Return hospital name in that state with lowest 30-day death rate
+  dataset = dataset[order(dataset[,2]),]
+  last_row = nrow(dataset)
+  if(tolower(num)=="best"){
+    name = dataset[1,1]
+  }else if(tolower(num)=="worst"){
+    name = dataset[last_row,1]
+  }else{
+    num = as.integer(num)
+    if(num>last_row){
+      name = NULL
+    }else{
+      name = dataset[num,1]
+    }
+  }
+  name
 }
 
