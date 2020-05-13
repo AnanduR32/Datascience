c10a828d22b131500e5354ecc8c473e13fd185df
Anandu R <ananduraj32@gmail.com>
2020-05-13 18:53:32 +0530

    Update base_plotting.R

--- Basics/Plotting/base_plotting.R
+++ Basics/Plotting/base_plotting.R
@@ -1,4 +1,4 @@
-## Using base plotting package to plot the airquality data 
+## ------------------------ Using base plotting package to plot the airquality data ---------------------------- ## 
 ## for month of may against all other months 
 
 ## Creating a plot, but plotting no points on the plot
@@ -12,3 +12,17 @@ with(subset(airquality, Month!=5), points(Wind, Ozone, col = "red"))
 
 ## Adding legend to the graph to communicate the information in the graph
 legend("topright", pch = 1, col = c("blue","red"), legend = c("May","Other months"))
+
+## ------------------------------ Adding linear regression line to the plot ------------------------------------ ##
+## Creating the model
+model = lm(Ozone~Wind, airquality)
+
+## Adding the line 
+abline(model,lwd = 2, col = "purple")
+
+## ----------------------------------------- Multiple base plot ------------------------------------------------ ##
+par(mfrow = c(1,2))
+with(airquality,{
+  plot(Wind, Ozone, main = "Ozone and Wind")
+  plot(Solar.R, Ozone, main = "Ozone and Solar Radiation")
+})
