8c6b3e7c6bd512d9aaf6c978e37377b635d03aaa
Anandu R <ananduraj32@gmail.com>
2020-06-15 22:41:21 +0530

    kmeans clustering

--- navigation.R
+++ navigation.R
@@ -9,8 +9,18 @@ setwd("Case Studies")
 # Navigate to Basics
 setwd("Basics")
 
+# Navigate to Clustering 
+setwd("Basics/Clustering/rmd")
+
 # Navigate to plotting
 setwd("Basics/Plotting/rmd")
 
 ## Rendering to PDF - rmarkdown
+# Plotting
 rmarkdown::render("ggplot.Rmd",output_file = "../pdf/ggplot.pdf")
+
+# Heirarchical Clustering
+rmarkdown::render("hierarchical_clustering.Rmd",output_file = "../hierarchical_clustering.pdf")
+
+# K-Means Clustering
+rmarkdown::render("k-means_clustering.Rmd",output_file = "../k-means_clustering.pdf")
